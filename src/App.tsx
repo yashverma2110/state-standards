2e958a784166f509bceb26017a4e4e4938c3b820
yash2110 <yash@quizizz.com>
2022-04-26 14:11:40 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import "./App.css";
+import { API } from "./utils/api.config";
 
 function App() {
+  useEffect(() => {
+    getAllJusrisdictions();
+  }, []);
+
+  const getAllJusrisdictions = async () => {
+    const data = await API.get("jurisdictions");
+
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h5" component="h1">
+        Available jurisdictions
+      </Typography>
     </div>
   );
 }
