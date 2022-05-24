150c4fd6a5a32b70a36f4b3cd9737eccb5f5bac1
yash2110 <yash@quizizz.com>
2022-05-24 14:26:02 +0530

    fix(api/config): changed base url to aws client

--- src/utils/api.config.ts
+++ src/utils/api.config.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://state-standards-be.vercel.app",
+  baseURL:
+    process.env.NODE_ENV === "dev"
+      ? "http://localhost:8080"
+      : "http://34.227.222.221:8080",
 });
