d864eb100e8845ef5c5f5cc32d58cc3cdec0bd43
yashverma2110 <yash.silver2110@gmail.com>
2022-09-09 19:04:35 +0530

    fix: domain name for backend

--- src/utils/api.config.ts
+++ src/utils/api.config.ts
@@ -2,11 +2,10 @@ import axios from "axios";
 
 export const API = axios.create({
   baseURL:
-    process.env.NODE_ENV === "dev"
-      ? "http://localhost:8080"
-      : "https://dev-services.quizizz.com",
+    process.env.NODE_ENV === 'dev'
+      ? 'http://localhost:8080'
+      : 'https://dev.quizizz.com/_ssserver',
   headers: {
-    "x-standard": "state",
-    "Access-Control-Allow-Origin": "*",
+    'Access-Control-Allow-Origin': '*',
   },
 });
