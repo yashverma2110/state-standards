72317bc7244dfbcc0f3d02f9ec00dd9625e0bc90
yashverma2110 <yash.silver2110@gmail.com>
2022-09-09 20:08:31 +0530

    feat: improved search result

--- src/views/AllJurisdiction.tsx
+++ src/views/AllJurisdiction.tsx
@@ -74,8 +74,11 @@ const AllJurisdiction = ({ setAlert, setView }: AllJurisdictionProps) => {
       setplaces(allData);
       return;
     }
-
-    const placesToSearch = places.filter((place) => {
+    
+    const placesToSearch = allData.filter((place) => {
+      if (typeFilter !== 'all' && place.type !== typeFilter) {
+        return false;
+      }
       return (
         place.title.toLowerCase().indexOf(e.target.value?.toLowerCase()) > -1
       );
